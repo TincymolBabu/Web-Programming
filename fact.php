4c860582507105d5e2912fbb620bbdd5b336565d
TincymolBabu <117815480+TincymolBabu@users.noreply.github.com>
2023-01-13 11:08:57 +0530

    Add files via upload

--- fact.php
+++ fact.php
@@ -1,25 +1,23 @@
-<html>
-<head>
-<title>FACTORIAL PROGRAM USIG FOR LOOP IN PHP<title>
-</head>
-<body>
-<form methos ="POST">
-enter the number:<br>
-<input type="number" name="number" id="number">
-<input type="submit" name="submit" value="submit">
-</form>
-<?php
-if($_POST)
-{
-$fact=1;
-$number=$_POST['number'];
-echo "factorial of a nummber:<br><br>";
-for($i=0;$i<=number;$i++)
-{
-$fact=$fact*$i;
-}
-echo $fact"<br>";
-}
-?>
-</body.
-</html>
+<html>  
+<head>  
+<title>Factorial Program using loop in PHP</title>  
+</head>  
+<body>  
+<form method="post">  
+    Enter the Number:<br>  
+    <input type="number" name="number" id="number">  
+    <input type="submit" name="submit" value="Submit" />  
+</form>  
+<?php   
+    if($_POST){  
+        $fact = 1;  
+        $number = $_POST['number'];  
+        echo "Factorial of $number:<br><br>";  
+       for ($i = 1; $i <= $number; $i++){         
+            $fact = $fact * $i;  
+            }  
+            echo $fact . "<br>";  
+    }  
+?>  
+</body>  
+</html>  
